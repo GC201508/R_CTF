457e30d58207ec08a883d6e7c3ec24cc04444dec
Z.Take <kbc15b08@stu.kawahara.ac.jp>
2017-11-02 09:52:10 +0900

    Add
    GameManagerオブジェクトとスクリプト追加

---
+++ Rocket_CaptureTheFlag/Assets/00_Master/04_Script/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*	-	-	-	-	-	-	-	-	-	-	-
+ 
+	・シーンを遷移しても保持すべきデータを管理する.
+ 
+ -	-	-	-	-	-	-	-	-	-	-	*/
+
+public class GameManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
