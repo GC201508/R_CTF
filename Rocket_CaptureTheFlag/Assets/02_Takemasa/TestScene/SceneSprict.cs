f1cffefa29117005270ec1cb9a90da14ec14cdd8
Z.Take <kbc15b08@stu.kawahara.ac.jp>
2017-11-06 22:27:09 +0900

    Add(TakemasaFolder)
    動作確認で使うSceneとかスクリプト追加

---
+++ Rocket_CaptureTheFlag/Assets/02_Takemasa/TestScene/SceneSprict.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+/*	-	-	-	-	-	-	-	-	
+ 
+	・シーン遷移作るよ！！！！！！！！！！１　会釈！！！！！！！
+ 
+ -	-	-	-	-	-	-	-	-*/
+
+public class SceneSprict : MonoBehaviour {
+
+	public string nextSceneName;	//遷移先のシーン名入れてね！！！！！！！！！！！！！！！！！！！！！！
+
+
+	// Use this for initialization
+	void Start () {
+		
+		//呼び出したら使えるようにするよ！！！！！！！！　はしゃぎすぎ！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！
+		SceneManager.LoadScene(nextSceneName);
+	}
+	
+	// Update is called once per frame 多分使わない！！！！！！！！！！！！！！！！！
+	void Update () {
+		
+	}
+}
