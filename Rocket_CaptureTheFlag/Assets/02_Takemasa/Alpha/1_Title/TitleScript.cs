23ded4fc8ad9ffe71482c516bc41988280d9f6e3
Z.Take <kbc15b08@stu.kawahara.ac.jp>
2017-11-20 14:30:42 +0900

    update & add (TakemasaFolder)
    [update]
    ・α版完成
    
    [add]
    ・ステージエディター作成の為　作業用フォルダ作成
    ・ステージエディターに使用する画像の追加

---
+++ Rocket_CaptureTheFlag/Assets/02_Takemasa/Alpha/1_Title/TitleScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* -	-	-	-	-	-	-	
+ 
+	・タイトルの制御会釈！！！！！！！会釈！！！！！！！　レスキュー！！！！！！！！！！！！！！！！！！
+
+ -	-	-	-	-	-	-	*/
+
+public class TitleScript : MonoBehaviour {
+
+	public GameObject sceneManager;	//Entryに移行する用設定したオブジェクトダンス隊登場！！！！！
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//スタートボタンでsceneManagerをActiveTrue会釈！！
+		if(Input.GetKeyDown(KeyCode.Joystick1Button7))
+		{
+			sceneManager.SetActive(true);
+		}
+	}
+}
---
+++ Rocket_CaptureTheFlag/Assets/02_Takemasa/Alpha/2_Entry/ColorSprict.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*	-	-	-	-	-	-	-	-	-	
+ 
+	・オブジェクトの色変えるスクリプト会釈！！！！！！！
+	・プレイヤーEntryシーンで使うとアイ・アムうまいアム！！！！！　レスキュー！！レスキュー！！
+	・対応したロケットの色変えたりシーンの移動したりはマスター版でEntryManager使ってやってね。これテストプログラムだから。　オゾリンナ。
+
+-	-	-	-	-	-	-	-	-	*/
+
+public class ColorSprict : MonoBehaviour {
+
+	public GameObject sceneManager; //ステージセレクトに遷移する状態にしたシーンマネージャ入れてね.
+	bool isEntry = false;	//エントリー状態か否か,
+
+	// Use this for initialization
+	void Start () {
+		//選択されてない状態の色は灰色で透けさせる.
+		GetComponent<SpriteRenderer>().color = new Color(0.5f,0.5f,0.5f,0.7f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//シーンActiveにしてステージセレクトに遷移させる.
+		if (Input.GetKeyDown(KeyCode.Joystick1Button7) && isEntry)
+		{ 
+			sceneManager.SetActive(true);	
+		}
+
+		//コントローラのスタートボタンの入力があったら色の状態を戻す.		
+		if (Input.GetKeyDown(KeyCode.Joystick1Button7) && !isEntry)
+		{ 
+			GetComponent<SpriteRenderer>().color = new Color(1.0f,1.0f,1.0f,1.0f);
+			isEntry = true;
+		}
+
+	}
+}
