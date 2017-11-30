82835c0b2c326afeb18f6c8cce0ca5c086e0b9f6
Z.Take <kbc15b08@stu.kawahara.ac.jp>
2017-11-30 11:07:57 +0900

    update

---
+++ Rocket_CaptureTheFlag/Assets/02_Takemasa/Alpha/3/SelectConfig.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*	-	-	-	-	-	-	-	-	-	-	-	
+	
+	・ステージの選択を行った後シーンを移動する.
+	・ここではSceneManagerを使ったシーン遷移を行わない.
+	
+	＜予定＞
+	・各プレイヤーが保有しているポイントを表示する.
+ 
+ -	-	-	-	-	-	-	-	-	-	-	*/
+
+public class SelectConfig : MonoBehaviour {
+
+	public GameObject[] slectStage;	//選択するステージの黒いほう.
+	public struct StageContent {
+		public GameObject Stage;
+		public bool isSelect;
+	}
+	
+	List<StageContent> listStageContent = new List<StageContent>();
+
+	// Use this for initialization
+	void Start () {
+		foreach (GameObject go in slectStage)
+		{ 
+			StageContent sc;
+			sc.Stage = go;
+			sc.isSelect = false;
+
+			listStageContent.Add(sc);
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
