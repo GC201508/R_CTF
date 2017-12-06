96301d96eb45245ce3a2778c04610efba73bf63c
Z.Take <kbc15b08@stu.kawahara.ac.jp>
2017-12-06 14:57:37 +0900

    add
    ScoreConfig(スクリプト)
    ScoreConfig(ゲームオブジェクト)を追加.
    
    スコアボードのプレハブ作った.

---
+++ Rocket_CaptureTheFlag/Assets/02_Takemasa/Alpha/3/ScoreConfig.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+/*
+
+	・SelectStageSceneで使うよ。　SSSだわ.
+	・スコアの表示しまーす.
+
+	・プレイヤー毎にRocketのスプライト変えるから.
+	・スコアはGameConfigに教えてもらってね.
+	
+	・プレイヤーの参加人数で作成するスコアボード変わるわ.
+		=>横等分になるようにしてね.
+ 
+ */
+public class ScoreConfig : MonoBehaviour {
+
+	public Text score;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
