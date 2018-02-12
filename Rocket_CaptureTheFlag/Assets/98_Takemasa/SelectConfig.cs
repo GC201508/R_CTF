85ff72ba23c48309fe1fd5fc30bac7bd54e901cd
Z.Take <kbc15b08@stu.kawahara.ac.jp>
2018-02-13 05:38:26 +0900

    update
    ・ステージSelectの完成
    
    ・ステージ３を大幅に調整.
    ※開始地点とゴールポイントが近い場合、カメラ追従が極端に遅れてしまう挙動になる為、ゴールポイントを離しプレイに支障が出ないようにした

--- Rocket_CaptureTheFlag/Assets/98_Takemasa/SelectConfig.cs
+++ Rocket_CaptureTheFlag/Assets/98_Takemasa/SelectConfig.cs
@@ -2,21 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.UI;
 /*	-	-	-	-	-	-	-	-	-	-	-	
 	
 	・ステージの選択を行った後シーンを移動する.
 	・ここではSceneManagerを使ったシーン遷移を行わない.
 		=>ステージ名 ＝ オブジェクト名とし、選択状態のオブジェクトからシーン遷移を行う.
+	・SelectItem'sCountText（UI Text）を項目に応じて変更する.
 	
+	
+	・総ステージは １０ステージ　で確定とし、　変更は絶対に受け付けないものとする。
+	・1度に画面内に表示する選択数は　３つ　で確定とし、　　変更は絶対に無い。　無いったら無い.	
+	
+
 	（スコア表示関連はScoreConfigが担う。　スコア処理のコード不要）
  
  -	-	-	-	-	-	-	-	-	-	-	*/
 
 public class SelectConfig : MonoBehaviour
 {
+	public Text selectItemCntText; //項目表示するText.
+	public RectTransform rtParentIcon;	//子クラスにIconをもつオブジェクトのRectTransform.
 
 	public GameObject[] slectStage; //シーン遷移先の名前と同等のオブジェクトを選ぶ(Iconの子クラスにあるよ).
-
+	
 	//ステージ選択に使う構造体.
 	public struct StageContent
 	{
@@ -112,6 +121,8 @@ public class SelectConfig : MonoBehaviour
 		{
 			SetActiveSC(listStageContent[selectNum + changeNum], false); //選択前のステージをoffに.
 			SetActiveSC(listStageContent[selectNum], true); //選択後のステージをtrueに.
+
+			ChangeTextItemCount();  //項目数の変更や表示領域の移動を行う(必要ならば).
 		}
 
 	}
@@ -124,5 +135,57 @@ public class SelectConfig : MonoBehaviour
 		{
 			SceneManager.LoadScene(listStageContent[selectNum].Stage.name);
 		}
+	}
+
+	/*ステージの項目数を表示するUI Textの値を変更する.
+			=>MoveIconEmpty関数で選択領域の移動も行う. 
+	 *ステージ項目数は必ず１０とする.
+	 *変更するテキスト　"◀xxxOxx/xx4xxx▶"*/
+	void ChangeTextItemCount()
+	{
+		int moveCnt = 0;	//どれだけ移動するかをwidthSizeに乗算する.		
+	
+		//１項目参照	1/4
+		if (selectNum <= 2)
+		{
+			selectItemCntText.text = "    1  /  4   ▶";
+		}
+
+		//２項目参照	2/4
+		else if (selectNum <= 5)
+		{
+			selectItemCntText.text = "◀   2  /  4   ▶";
+			moveCnt = 1;
+			
+		}
+
+		//３項目参照	3/4
+		else if (selectNum <= 8)
+		{
+			selectItemCntText.text = "◀   3  /  4   ▶";
+			moveCnt = 2;
+		}
+
+		//４項目参照	4/4
+		else
+		{
+			selectItemCntText.text = "◀   4  /  4    ";
+			moveCnt = 3;
+		}
+
+		MoveIconEmpty(moveCnt); //移動させる.
+	}
+
+	/*項目数に応じて選択領域の表示を移動させるための関数.
+	 *第1引数：表示している項目 - 1 の数字(moveCnt).
+	 *ChangeTextItemCount関数内で使用する.*/
+	void MoveIconEmpty(int cnt)
+	{
+		float widthSize = 800; //横スクリーンサイズ。これが移動量になる.
+		Vector3 toPos = rtParentIcon.localPosition;	//値受け渡し用変数.
+
+		toPos.x = widthSize * cnt * -1; //cntに応じた位置に移動.
+
+		rtParentIcon.localPosition = toPos;	//値渡して終了.
 	}
 }
